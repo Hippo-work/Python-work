dde3bd13bb8dd23fae84fb69d3c312a0386f9208
James <hippophantez@gmail.com>
2025-08-07 22:07:27 +0100

    again

--- XOR_bits.py
+++ XOR_bits.py
@@ -5,7 +5,7 @@ import sys
 def xor_file(*contents):
     for content in contents:
         input_content = bytes(content)
-        xor_content = bytes(b ^ 0b1 for b in content)
+        xor_content = bytes(b ^ 0xFF for b in content)
    # print(f"input content:    {input_content.hex()} Binary: {' '.join(f'{byte:08b}' for byte in input_content)}")  # Print the first 10 bytes of the input content
     print(f"XOR content:      {xor_content.hex()} Binary: {' '.join(f'{byte:08b}' for byte in xor_content)} bytes xor from {buffer}.")  # Print the first byte of the inverted content
 
